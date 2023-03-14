21277a6db2e284b56897a1a0114955e295b1cee4
mariuszbyczek <91824400+mariuszbyczek@users.noreply.github.com>
2023-03-14 13:34:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import populate_db
 from library import Library
 
 
 if __name__ == '__main__':
     library = Library('library.db')
+    populate_db.create_test_data(library)
+    all_books = library.get_all_books()
+    all_borrowers = library.get_all_borrowers()
+    all_borrows = library.get_all_borrowed_books()
